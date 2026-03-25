d739a093e5e23e31b8e536e833c5e5f14c399923
Diego Fernando <fercris1124@gmail.com>
2026-03-25 10:37:57 -0500

    Configuracion para despliegue en render

--- index.js
+++ index.js
@@ -10,10 +10,10 @@ app.set('port', process.env.PORT || 3000);
 require("./sockets")(io);
 app.use(express.static(path.join(__dirname,'src/Cliente')));
 
-server.listen(app.get('port'),()=>{
+server.listen(app.get('port'),'0.0.0.0',()=>{
     console.log("Servidor en el puesto",app.get('port'));
 })
-
-mongoose.connect('mongodb://127.0.0.1/chat-database')
+const dbUri=process.env.MONGODB_URI || 'mongodb://127.0.0.1/chat-database'
+mongoose.connect(dbUri)
     .then(()=>console.log("base de datos conectada"))
     .catch(err => console.log("Error en DB",err))
